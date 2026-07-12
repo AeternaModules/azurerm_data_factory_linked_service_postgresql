c7c0847da164f4149fd29c8d2efb39ae0dc3e15d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "data_factory_linked_service_postgresqls" {
-  description = "All data_factory_linked_service_postgresql resources"
-  value       = azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls
-}
 output "data_factory_linked_service_postgresqls_additional_properties" {
-  description = "List of additional_properties values across all data_factory_linked_service_postgresqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_linked_service_postgresqls, keyed the same as var.data_factory_linked_service_postgresqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : k => v.additional_properties }
 }
 output "data_factory_linked_service_postgresqls_annotations" {
-  description = "List of annotations values across all data_factory_linked_service_postgresqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_service_postgresqls, keyed the same as var.data_factory_linked_service_postgresqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : k => v.annotations }
 }
 output "data_factory_linked_service_postgresqls_connection_string" {
-  description = "List of connection_string values across all data_factory_linked_service_postgresqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : v.connection_string]
+  description = "Map of connection_string values across all data_factory_linked_service_postgresqls, keyed the same as var.data_factory_linked_service_postgresqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : k => v.connection_string }
 }
 output "data_factory_linked_service_postgresqls_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_service_postgresqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_service_postgresqls, keyed the same as var.data_factory_linked_service_postgresqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : k => v.data_factory_id }
 }
 output "data_factory_linked_service_postgresqls_description" {
-  description = "List of description values across all data_factory_linked_service_postgresqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : v.description]
+  description = "Map of description values across all data_factory_linked_service_postgresqls, keyed the same as var.data_factory_linked_service_postgresqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : k => v.description }
 }
 output "data_factory_linked_service_postgresqls_integration_runtime_name" {
-  description = "List of integration_runtime_name values across all data_factory_linked_service_postgresqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : v.integration_runtime_name]
+  description = "Map of integration_runtime_name values across all data_factory_linked_service_postgresqls, keyed the same as var.data_factory_linked_service_postgresqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : k => v.integration_runtime_name }
 }
 output "data_factory_linked_service_postgresqls_name" {
-  description = "List of name values across all data_factory_linked_service_postgresqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : v.name]
+  description = "Map of name values across all data_factory_linked_service_postgresqls, keyed the same as var.data_factory_linked_service_postgresqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : k => v.name }
 }
 output "data_factory_linked_service_postgresqls_parameters" {
-  description = "List of parameters values across all data_factory_linked_service_postgresqls"
-  value       = [for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_service_postgresqls, keyed the same as var.data_factory_linked_service_postgresqls"
+  value       = { for k, v in azurerm_data_factory_linked_service_postgresql.data_factory_linked_service_postgresqls : k => v.parameters }
 }
 
